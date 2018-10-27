dd57640de8f45ba2cb15bf7970bba219d322976d
JustKshitijD <44445148+JustKshitijD@users.noreply.github.com>
2018-10-27 08:54:05 +0530

    added corner cases for insertAtPosition(),delete(),deleteFirstElement()
    tail and head pointers will have to be updated properly.Check if the position you require is going out of list.

--- LinkedListCPP.cpp
+++ LinkedListCPP.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
 
 // You should use templates when you do not know what type of data will be
 // stored in List
@@ -37,6 +39,7 @@ void List<T>::createNode(T value)
     if (listHead == nullptr) {
         listHead = tmp;
         listTail = tmp;
+        
         tmp = nullptr;
     } else {
         listTail->nextNode = tmp;
@@ -56,20 +59,35 @@ void List<T>::insertValue(T value)
 template<typename T>
 void List<T>::insertValueAtPosition(int pos, T value)
 {
+    int i;
     Node<T>* pre = new Node<T>;
     Node<T>* current = new Node<T>;
     Node<T>* tmp = new Node<T>;
 
     current = listHead;
 
-    for (int i = 1; i < pos; i++) {
+    for ( i = 1; i < pos; i++) {
+        if(current==NULL)
+            break;
         pre = current;
         current = current->nextNode;
     }
-
+   if(pos-i>1)
+   {
+       cout<<"Position out of reach";
+   }
+    else if(pos-i==1)//when element is to be inserted at end
+    {
+        tmp->data=value;
+        listTail->next=tmp;
+        listTail=tmp;
+    }
+    else
+    {
     tmp->data = value;
     pre->nextNode = tmp;
     tmp->nextNode = current;
+    }
 }
 
 template<typename T>
@@ -77,9 +95,16 @@ void List<T>::deleteFirstElement()
 {
     Node<T>* tmp = new Node<T>;
     tmp = listHead;
+    if(listHead==listTail)
+    {
+        listHead=NULL;listTail=NULL;
+        delete(tmp);
+    }
+    else{
     listHead = listHead->nextNode;
     
     delete tmp;
+    }
 }
 
 template<typename T>
@@ -94,32 +119,62 @@ void List<T>::deleteLastElement()
         previous = current;
         current = current->nextNode;
     }
-
+    if(current==listHead)
+    {
+        Node<T>*temp=listHead;
+        listHead=NULL;listTail=NULL;
+        delete(temp);
+    }
+    else
+    {
     listTail = previous;
     previous->nextNode = nullptr;
     
     delete current;
+    }
 }
 
 template<typename T>
 void List<T>::deleteElementAtPosition(int pos)
 {
-    Node<T>* current = new Node<T>;
-    Node<T>* previous = new Node<T>;
-
-    current = listHead;
-
-    for (int i = 1; i < pos; i++) {
-        previous = current;
-        current = current->nextNode;
+    Node<T>* current=listHead;Node<T>*prev;//taking that first nodeis at position zero
+    for(int i=0;i<pos-1;i++)
+    {
+        
+        if(current==NULL)
+        {
+            cout<<"Position out of reach";
+            break;
+        }
+       prev=current; 
+       current=current->nextNode; 
     }
-    previous->nextNode = current->nextNode;
+    if(current!=NULL)
+    {
+        if(current==listHead)
+        {
+            listHead=current->nextNode;
+            
+        }
+        else if(current==listTail)
+        {
+            listTail=prev;
+            prev->nextNode=current->nextNode;
+        }
+        else
+        {
+        prev->nextNode=current->nextNode;
+        }
+        delete(current);
+        
+    }
+       
 }
 
 template<typename T>
 void List<T>::showElementsOfList()
 {
-    Node<T>* tmp = new Node<T>;
+    Node<T>* tmp ;
     tmp = listHead;
 
     while (tmp != nullptr) {
